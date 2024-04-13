c392970f4d6ea63b563dcaa7ce0a82d8728835b4
chaitanya586 <107758530+chaitanya586@users.noreply.github.com>
2024-04-13 21:39:15 +0530

    Update 6674 TT-7.cpp

--- 6674 TT-7.cpp
+++ 6674 TT-7.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 long repeatedString(string s, long n) {
-    // Count the number of 'a' in the given string s
+   
     long count_a_in_s = 0;
     for (char ch : s) {
         if (ch == 'a') {
@@ -11,13 +11,12 @@ long repeatedString(string s, long n) {
         }
     }
 
-    // Calculate the number of times the string s repeats completely in the first n characters
     long complete_repeats = n / s.length();
 
     // Calculate the remaining characters after complete repeats
     long remaining_chars = n % s.length();
 
-    // Count the number of 'a' in the remaining characters
+   
     long count_a_in_remaining = 0;
     for (int i = 0; i < remaining_chars; i++) {
         if (s[i] == 'a') {
@@ -25,7 +24,7 @@ long repeatedString(string s, long n) {
         }
     }
 
-    // Calculate the total count of 'a' in the first n characters
+  
     long total_count_a = (complete_repeats * count_a_in_s) + count_a_in_remaining;
 
     return total_count_a;
@@ -36,7 +35,7 @@ int main() {
     string s = "aba";
     long n = 10;
 
-    // Call the repeatedString function
+
     long result = repeatedString(s, n);
 
     // Output the result
